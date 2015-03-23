3198efbf67c3cdf0f2f7d834613b0447e56b4d3b
Gabor Balazs <gabalz@gmail.com>
2015-03-22 23:16:22 -0600

    Initial commit.

---
+++ test/jvecmat/AssertionBaseTest.java
@@ -0,0 +1,17 @@
+package jvecmat;
+
+import junit.framework.TestCase;
+
+/**
+ * Enable the implementation assertions.
+ */
+public class AssertionBaseTest extends TestCase {
+
+    public AssertionBaseTest(String name) {
+        super(name);
+    }
+
+    static {
+        ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
+    }
+}
