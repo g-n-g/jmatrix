66e8b2fcdc2ff57f85c1f56fbab7cb5292f9436b
Gabor Balazs <gabalz@gmail.com>
2023-11-25 15:28:08 +0100

    Removing old locations.

--- src-bench/jmatrix/bench/BenchmarkException.java
+++
@@ -1,8 +0,0 @@
-package jmatrix.bench;
-
-public class BenchmarkException extends Exception
-{
-  public BenchmarkException(String message) {
-    super(message);
-  }
-}
--- src/jmatrix/package-info.java
+++
@@ -1,7 +0,0 @@
-
-/**
- * A vector/matrix library which optimizes performance through polymorphism
- * and allows caching the (final/intermediate) results to reduce the overhead
- * of garbage collection.
- */
-package jmatrix;
